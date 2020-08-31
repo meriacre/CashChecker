6c31e478c8343359c9674c289dd1488cd8f41e12
Andrew <fantasy_all4me@yahoo.com>
2020-08-31 18:35:26 +0300

    1. Finisarea functiei de calcul a banilor

--- app/src/main/java/md/merit/strangatorul/MainActivity.kt
+++ app/src/main/java/md/merit/strangatorul/MainActivity.kt
@@ -1,31 +1,40 @@
 package md.merit.strangatorul
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.lo_add_money.*
+import kotlinx.android.synthetic.main.lo_add_money.view.*
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
-    companion object{
+    companion object {
         lateinit var dbHandler: DBHandler
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         dbHandler = DBHandler(this, null, null, 1)
-
         viewTransactions()
+       loadData()
 
-
-        addItem.setOnClickListener{
-    val intent = Intent(this, AddItemActivity::class.java)
-    startActivity(intent)
-}
+        addItem.setOnClickListener {
+            val intent = Intent(this, AddItemActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 
     override fun onResume() {
         viewTransactions()
@@ -33,23 +42,64 @@ class MainActivity : AppCompatActivity() {
         edtRest.setText(returnRest().toString())
     }
 
-private fun viewTransactions(){
-    val transactionList = dbHandler.getTransactions(this)
-    val adapter = MyAdapter(this,transactionList)
-    val rv : RecyclerView = findViewById(R.id.recycler_view)
-    rv.layoutManager = LinearLayoutManager(this)
-    rv.adapter = adapter
-}
+    private fun viewTransactions() {
+        val transactionList = dbHandler.getTransactions(this)
+        val adapter = MyAdapter(this, transactionList)
+        val rv: RecyclerView = findViewById(R.id.recycler_view)
+        rv.layoutManager = LinearLayoutManager(this)
+        rv.adapter = adapter
+    }
 
     private fun returnRest(): Double {
         val sumTransactions = dbHandler.returnSumPrice()
         var totalMoney: Double
-        if(edtTotal.text.toString().equals(""))
-            totalMoney =0.0
+        if (edtTotal.text.toString().equals(""))
+            totalMoney = 0.0
         else
             totalMoney = edtTotal.text.toString().toDouble()
-        var rest = totalMoney - sumTransactions
-        return rest
+        return totalMoney - sumTransactions
+    }
+
+    fun addMoney(view: View) {
+        val builder = AlertDialog.Builder(this)
+        val inflater = layoutInflater
+        builder.setTitle("Please write your money amount!")
+        val dialogLayout = inflater.inflate(R.layout.lo_add_money, null)
+        val editText = dialogLayout.findViewById<EditText>(R.id.edtTotalMoney)
+
+        builder.setView(dialogLayout)
+        builder.setIcon(R.drawable.ic_baseline_attach_money_24)
+        builder.setPositiveButton("OK") { dialogInterface, i ->
+            try {
+                val a = dialogLayout.edtTotalMoney.text.toString()
+                edtTotal.text = a
+                saveData()
+            }catch (e:Exception){
+                Toast.makeText(this, "Data can not be saved!", Toast.LENGTH_SHORT).show()
+            }
+        }
+        builder.setNegativeButton("Cancel") { dialogInterface, i ->
+
+        }
+        builder.show()
+    }
+
+    private fun saveData(){
+        val insertedValue = edtTotal.text.toString()
+
+        val sharedPref = getSharedPreferences("sharedPref", Context.MODE_PRIVATE)
+        val editor = sharedPref.edit()
+        editor.apply(){
+            putString("STRING_KEY", insertedValue)
+        }.apply()
+        
+        Toast.makeText(this, "Your amount of money are saved!", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun loadData(){
+        val sharedPref = getSharedPreferences("sharedPref", Context.MODE_PRIVATE)
+        val savedString = sharedPref.getString("STRING_KEY",null)
+        edtTotal.text = savedString
     }
 
 }
